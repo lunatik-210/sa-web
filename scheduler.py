296f0ef939e9cb060d0e0a4f98ccc1aa0ed98f6f
Andrew Lapin <andrew.d.lapin@gmail.com>
2012-05-27 18:22:59 +0400

    Added sending email oprion

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ dot      = 'dot'
 
 def execute(file, input):
 	from subprocess import Popen, PIPE, STDOUT
-	
+
 	p = Popen(file, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 	return p.communicate(input=input)[0]
 
@@ -18,6 +18,20 @@ def make_call_graph(filename):
 	output = execute(sacparse, stream)
 	output = execute(sacg2dot, output)
 	output = execute([dot, '-Tsvg', '-o', 'out.svg'], output)
+
+def send_email(to, msg, file):
+	import smtplib
+	from email.mime.text import MIMEText
+	
+	msg = MIMEText(msg)
+	msg['Subject'] = 'Your task is processed'
+	msg['From'] = 'poddy.org'
+	msg['To'] = to
+
+	s = smtplib.SMTP('localhost')
+	s.sendmail('noreply@sourceanalyzer.org', [to], msg.as_string())
+	s.quit()
+
 
 if __name__ == '__main__':
 	import MySQLdb as mysql
@@ -34,6 +48,8 @@ if __name__ == '__main__':
 		make_call_graph(results[0][2])
 		handler.execute('DELETE FROM requests WHERE id = %s' % results[0][0])
 		db.commit()
+
+		send_email('andrew.d.lapin@gmail.com', 'task is done', 'asd')
 
 		#just for testing
 		break
